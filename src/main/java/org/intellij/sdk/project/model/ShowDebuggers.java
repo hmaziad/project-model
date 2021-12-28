d9855307ced71c2f8781ebd6f57d1f2d83ec6d8a
hmaziad <hmaziad@murex.com>
2021-12-28 12:11:56 +0200

    initial commit

---
+++ src/main/java/org/intellij/sdk/project/model/ShowDebuggers.java
@@ -0,0 +1,67 @@
+// Copyright 2000-2020 JetBrains s.r.o. and other contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+
+package org.intellij.sdk.project.model;
+
+import com.intellij.debugger.engine.JavaStackFrame;
+import com.intellij.debugger.ui.impl.watch.NodeManagerImpl;
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.application.ApplicationManager;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.project.ProjectManagerListener;
+import com.intellij.openapi.roots.ProjectRootManager;
+import com.intellij.openapi.ui.Messages;
+import com.intellij.openapi.util.Pair;
+import com.intellij.openapi.vfs.VirtualFile;
+import com.intellij.ui.SimpleTextAttributes;
+import com.intellij.xdebugger.XDebuggerManager;
+import com.intellij.xdebugger.attach.XAttachDebuggerProvider;
+import com.intellij.xdebugger.frame.XCompositeNode;
+import com.intellij.xdebugger.frame.XDebuggerTreeNodeHyperlink;
+import com.intellij.xdebugger.frame.XStackFrame;
+import com.intellij.xdebugger.frame.XValue;
+import com.intellij.xdebugger.frame.XValueChildrenList;
+import com.intellij.xdebugger.frame.XValueContainer;
+import com.intellij.xdebugger.impl.XDebuggerManagerImpl;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import javax.swing.*;
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.Semaphore;
+import java.util.function.BiFunction;
+
+public class ShowDebuggers extends AnAction {
+    private static final long TIMEOUT_MS = 3_000;
+
+    @Override
+    public void actionPerformed(@NotNull final AnActionEvent event) {
+        XDebuggerManager manager = XDebuggerManager.getInstance(event.getProject());
+
+        var frame = manager.getCurrentSession().getCurrentStackFrame();
+        var children = XDebuggerTestUtil.collectChildren(frame);
+
+        List<XTestValueNode> computedNodes = new ArrayList<>();
+
+        for (var xValue : children) {
+            try {
+                computedNodes.add(XDebuggerTestUtil.computePresentation(xValue));
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+        System.out.println("Done");
+
+    }
+
+    @Override
+    public void update(@NotNull final AnActionEvent event) {
+        boolean visibility = event.getProject() != null;
+        event.getPresentation().setEnabled(visibility);
+        event.getPresentation().setVisible(visibility);
+    }
+
+}
