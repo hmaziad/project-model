1920c0c1a93a16d1c087658f9661f117dea57ddd
hmaziad <hmaziad@murex.com>
2022-06-19 15:43:56 +0300

    cleaning up

--- src/main/java/org/intellij/sdk/project/model/xnodes/XTestValueNode.java
+++ src/main/java/org/intellij/sdk/project/model/xnodes/XTestValueNode.java
@@ -4,7 +4,6 @@
 package org.intellij.sdk.project.model.xnodes;
 
 import java.util.List;
-import java.util.Optional;
 import java.util.concurrent.CompletableFuture;
 
 import javax.swing.*;
@@ -20,12 +19,7 @@ public class XTestValueNode extends XValueNodePresentationConfigurator.Configura
     private final CompletableFuture childNodeFuture;
     private XTestCompositeNode compositeNode;
     private CompletableFuture<List<XValue>> noChildrenFuture;
-    public @Nullable Icon myIcon;
-    public @NotNull String myName;
-    public @Nullable String myType;
-    public @NotNull String myValue;
-    public boolean myHasChildren;
-    public XFullValueEvaluator myFullValueEvaluator;
+    private @NotNull String myValue;
 
     public XTestValueNode(CompletableFuture childNodeFuture, CompletableFuture<List<XValue>> noChildrenFuture, XTestCompositeNode compositeNode) {
         this.childNodeFuture = childNodeFuture;
@@ -35,29 +29,18 @@ public class XTestValueNode extends XValueNodePresentationConfigurator.Configura
 
     @Override
     public void applyPresentation(@Nullable Icon icon, @NotNull XValuePresentation valuePresentation, boolean hasChildren) {
-        myIcon = icon;
-        myType = valuePresentation.getType();
         myValue = XValuePresentationUtil.computeValueText(valuePresentation);
-        myHasChildren = hasChildren;
-
         if (!myValue.startsWith("Collecting data")) {
             compositeNode.setValue(myValue);
             childNodeFuture.complete(null);
         }
-
         if (!hasChildren) {
             noChildrenFuture.complete(null);
         }
-
     }
 
     @Override
     public void setFullValueEvaluator(@NotNull XFullValueEvaluator fullValueEvaluator) {
-        myFullValueEvaluator = fullValueEvaluator;
-    }
-
-    @Override
-    public String toString() {
-        return "{" + Optional.ofNullable(myType).orElse("") + "} " + myValue;
+    // do nothing
     }
 }
