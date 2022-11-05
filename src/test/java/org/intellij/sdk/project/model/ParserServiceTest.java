dcb8f39c5233296ef838236c2ee68b2370d2f596
hmaziad <hmaziad@murex.com>
2022-11-05 09:16:46 +0200

    test parse string to node done

---
+++ src/test/java/org/intellij/sdk/project/model/ParserServiceTest.java
@@ -0,0 +1,46 @@
+package org.intellij.sdk.project.model;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import org.apache.commons.collections.CollectionUtils;
+import org.intellij.sdk.project.model.xnodes.XTestCompositeNode;
+import org.junit.Test;
+
+public class ParserServiceTest {
+
+    @Test
+    public void test_parse_strings_to_node_dfs() throws IOException {
+        assertParsingForFile("src/test/resources/nodeAsString/source.txt");
+        assertParsingForFile("src/test/resources/nodeAsString/destination.txt");
+    }
+
+    private void assertParsingForFile(String path) throws IOException {
+        List<String> lines = Files.readAllLines(Path.of(path));
+        ArrayList<XTestCompositeNode> diffNodes = new ArrayList<>();
+        XTestCompositeNode rootNode = ParserService.parseStringsToNode(lines, diffNodes);
+        assertNodeValues(rootNode.getChildren(), lines, new AtomicInteger(0));
+        assertTrue("Diff nodes is not empty", CollectionUtils.isEmpty(diffNodes));
+        System.out.println();
+    }
+
+    private void assertNodeValues(List<XTestCompositeNode> children, List<String> lines, AtomicInteger index) {
+        if (CollectionUtils.isNotEmpty(children)) {
+            for (var currentNode : children) {
+                String errorMessage = String.format("Current node of value %s does not equal %s", currentNode.getValue(), lines.get(index.get()));
+                String[] values = lines.get(index.getAndIncrement()).split(",");
+                assertEquals(errorMessage, currentNode.getContainer().toString(), values[1]);
+                assertEquals(errorMessage, currentNode.getNodeId(), values[2]);
+                assertEquals(errorMessage, currentNode.getValue(), values[3]);
+                assertNodeValues(currentNode.getChildren(), lines, index);
+            }
+        }
+    }
+}
