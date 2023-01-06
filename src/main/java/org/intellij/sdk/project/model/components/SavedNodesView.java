b8e3a390733f61bfaf959ce9aa46470e0adbf7de
hmaziad <hmaziad@murex.com>
2023-01-07 00:54:53 +0200

    fix layout for node settings page

--- src/main/java/org/intellij/sdk/project/model/components/SavedNodesView.java
+++ src/main/java/org/intellij/sdk/project/model/components/SavedNodesView.java
@@ -7,6 +7,7 @@ import java.util.Map;
 import java.util.Objects;
 
 import javax.swing.*;
+import javax.swing.border.EmptyBorder;
 import org.intellij.sdk.project.model.services.PersistencyService;
 import org.intellij.sdk.project.model.xnodes.DebugNode;
 import org.jetbrains.annotations.NotNull;
@@ -30,32 +31,67 @@ public class SavedNodesView extends DialogWrapper {
     protected JComponent createCenterPanel() {
         setSize(1300, 1100);
         Map<String, DebugNode> nodes = persistencyService.getNodes();
-        JPanel dialogPanel = new JPanel(new GridLayout());
+        JPanel dialogPanel = new JPanel(new GridBagLayout());
+//        dialogPanel.setPreferredSize(new Dimension(1300, 1100));
+//        dialogPanel.setSize(new Dimension(1300, 1100));
 
-        JPanel keysPanel = getKeysPanel();
-        JList<String> keysList = new JBList<>(nodes.keySet().toArray(new String[0]));
-        keysList.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+        JScrollPane scrollableNodesPanel = getScrollableNodesPanel();
+        JScrollPane scrollableKeysPanel = getScrollableKeysPanel(nodes, scrollableNodesPanel);
 
-        JScrollPane scrollableKeysPanel = new JBScrollPane();
-        scrollableKeysPanel.setMaximumSize(new Dimension(300, 1100));
-        scrollableKeysPanel.setBorder(BorderFactory.createTitledBorder("Saved Node names"));
-        scrollableKeysPanel.setViewportView(keysList);
+        GridBagConstraints gbc = new GridBagConstraints();
+        gbc.fill = GridBagConstraints.BOTH;
+        gbc.gridy = 0;
+        gbc.weighty = 1;
 
+        gbc.gridx = 0;
+        gbc.weightx = 0;
+        dialogPanel.add(scrollableKeysPanel, gbc);
 
-        JScrollPane scrollableNodesPanel = new JBScrollPane();
-        scrollableNodesPanel.setBorder(BorderFactory.createTitledBorder("Node Content"));
+        gbc.gridx = 1;
+        gbc.weightx = 0;
+        dialogPanel.add(getButtonsPanel(), gbc);
 
-        keysPanel.add(keysList);
-        keysList.addListSelectionListener(e -> onKeySelected(nodes, scrollableNodesPanel, keysList));
-
-        dialogPanel.add(scrollableKeysPanel);
-        dialogPanel.add(scrollableNodesPanel);
+        gbc.gridx = 2;
+        gbc.weightx = 1;
+        dialogPanel.add(scrollableNodesPanel, gbc);
         return dialogPanel;
     }
 
+    private JPanel getButtonsPanel() {
+        JPanel panel = new JPanel();
+        panel.setBorder(new EmptyBorder(50, 5, 0, 5));
+        BoxLayout boxLayout = new BoxLayout(panel, BoxLayout.Y_AXIS);
+        panel.setLayout(boxLayout);
+        panel.add(new JButton("Delete"));
+        panel.add(new JButton("Rename"));
+        return panel;
+    }
+
     @NotNull
-    private JPanel getKeysPanel() {
-        return new JPanel(new FlowLayout());
+    private JScrollPane getScrollableKeysPanel(Map<String, DebugNode> nodes, JScrollPane scrollableNodesPanel) {
+        JScrollPane scrollableKeysPanel = new JBScrollPane();
+        scrollableKeysPanel.setPreferredSize(new Dimension(350, 1100));
+        scrollableKeysPanel.setSize(new Dimension(350, 1100));
+        scrollableKeysPanel.setMinimumSize(new Dimension(350, 1100));
+        scrollableKeysPanel.setBorder(BorderFactory.createTitledBorder("Saved Node names"));
+
+        JList<String> keysList = new JBList<>(nodes.keySet().toArray(new String[0]));
+        keysList.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+        keysList.addListSelectionListener(e -> onKeySelected(nodes, scrollableNodesPanel, keysList));
+
+        JPanel keysPanel = new JPanel(new BorderLayout());
+        keysPanel.add(keysList, BorderLayout.WEST);
+        scrollableKeysPanel.setViewportView(keysPanel);
+
+        return scrollableKeysPanel;
+    }
+
+    @NotNull
+    private JScrollPane getScrollableNodesPanel() {
+        JScrollPane scrollableNodesPanel = new JBScrollPane();
+        scrollableNodesPanel.setBorder(BorderFactory.createTitledBorder("Node Content"));
+        scrollableNodesPanel.setPreferredSize(new Dimension(0, 1100));
+        return scrollableNodesPanel;
     }
 
     private void onKeySelected(Map<String, DebugNode> nodes, JScrollPane scrollableNodesPanel, JList<String> keysList) {
