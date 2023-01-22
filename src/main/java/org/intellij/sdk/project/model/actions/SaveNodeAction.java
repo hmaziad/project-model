40b155ba2d0619cecd962547f76fcf03eb4f0bfe
hmaziad <hmaziad@murex.com>
2023-01-22 16:13:13 +0200

    Save specific node fix

--- src/main/java/org/intellij/sdk/project/model/actions/SaveNodeAction.java
+++ src/main/java/org/intellij/sdk/project/model/actions/SaveNodeAction.java
@@ -1,16 +1,20 @@
 package org.intellij.sdk.project.model.actions;
 
 import org.intellij.sdk.project.model.components.handlers.NodeHandler;
+import org.intellij.sdk.project.model.components.handlers.ReachServices;
 import org.intellij.sdk.project.model.tree.components.DebugNode;
 import org.jetbrains.annotations.NotNull;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.xdebugger.impl.ui.tree.actions.XDebuggerTreeActionBase;
 import com.intellij.xdebugger.impl.ui.tree.nodes.XValueNodeImpl;
 
-public class SaveNodeAction extends XDebuggerTreeActionBase {
+public class SaveNodeAction extends XDebuggerTreeActionBase implements ReachServices {
     private final NodeHandler nodeHandler = new NodeHandler();
     @Override
     protected void perform(XValueNodeImpl node, @NotNull String nodeName, AnActionEvent e) {
-        this.nodeHandler.save(new DebugNode(node));
+        DebugNode root = new DebugNode();
+        root.add(new DebugNode(node));
+        this.nodeHandler.save(root);
+        COMPONENT_SERVICE.getDebugTreeManager().setRoot(root);
     }
 }
