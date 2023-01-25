ccda3a72b477a95439986ee3cb2ed4b98b3503c3
hmaziad <hmaziad@murex.com>
2023-01-25 23:34:15 +0200

    icon and text

--- src/main/java/org/intellij/sdk/project/model/tree/components/DebugTreeRenderer.java
+++ src/main/java/org/intellij/sdk/project/model/tree/components/DebugTreeRenderer.java
@@ -14,13 +14,6 @@ public class DebugTreeRenderer extends DefaultTreeCellRenderer {
     private static final String HTML_FORMAT = "<html>%s</html>";
     private static final String SPAN_FORMAT = "<span style='color:%s;'>%s</span>";
     private JBColor theColor = null;
-    private String text;
-    private Icon icon;
-
-    @Override
-    public String getText() {
-        return this.text;
-    }
 
     @Override
     public Color getBackground() {
@@ -30,11 +23,6 @@ public class DebugTreeRenderer extends DefaultTreeCellRenderer {
     @Override
     public Color getBackgroundNonSelectionColor() {
         return theColor;
-    }
-
-    @Override
-    public Icon getIcon() {
-        return this.icon;
     }
 
     @Override
@@ -53,32 +41,11 @@ public class DebugTreeRenderer extends DefaultTreeCellRenderer {
             String hexColor = String.format("#%02X%02X%02X", color.getRed(), color.getGreen(), color.getBlue());
             sb.append(String.format(SPAN_FORMAT, hexColor, text));
         }
-        this.text = String.format(HTML_FORMAT, sb);
+        setText(String.format(HTML_FORMAT, sb));
 
         if (Objects.nonNull(node.getIconPath())) {
-            this.icon = IconLoader.getIcon(node.getIconPath(), DebugTreeRenderer.class);
+            setIcon(IconLoader.getIcon(node.getIconPath(), DebugTreeRenderer.class));
         }
         return this;
     }
 }
-
-/*
-    @Override
-    public void customizeCellRenderer(@NotNull JTree tree, Object value, boolean selected, boolean expanded, boolean leaf, int row, boolean hasFocus) {
-        if (!(value instanceof DebugNode)) {
-            return;
-        }
-        DebugNode node = (DebugNode) value;
-        final SimpleColoredText simpleColoredText = new SimpleColoredText();
-        List<String> texts = node.getTexts();
-        List<Integer> colors = node.getColors();
-        for (int i = 0; i < texts.size(); i++) {
-            Integer rgb = colors.get(i);
-            Color color = Objects.isNull(rgb) ? Color.lightGray : new Color(rgb);
-            simpleColoredText.append(texts.get(i), new SimpleTextAttributes(STYLE_PLAIN, color));
-        }
-        simpleColoredText.appendToComponent(this);
-        if (Objects.nonNull(node.getIconPath())) {
-            setIcon(IconLoader.getIcon(node.getIconPath(), DebugTreeRenderer.class));
-        }
-    }*/
