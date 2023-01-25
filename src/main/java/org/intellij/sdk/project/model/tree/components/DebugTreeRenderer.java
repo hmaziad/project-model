4e17e417aadf29a403b97ee77857dbb1ad03a4be
hmaziad <hmaziad@murex.com>
2023-01-25 23:09:26 +0200

    colors made with html

--- src/main/java/org/intellij/sdk/project/model/tree/components/DebugTreeRenderer.java
+++ src/main/java/org/intellij/sdk/project/model/tree/components/DebugTreeRenderer.java
@@ -1,20 +1,68 @@
 package org.intellij.sdk.project.model.tree.components;
 
 
-import static com.intellij.ui.SimpleTextAttributes.STYLE_PLAIN;
-
 import java.awt.*;
 import java.util.List;
 import java.util.Objects;
 
 import javax.swing.*;
-import org.jetbrains.annotations.NotNull;
+import javax.swing.tree.DefaultTreeCellRenderer;
 import com.intellij.openapi.util.IconLoader;
-import com.intellij.ui.ColoredTreeCellRenderer;
-import com.intellij.ui.SimpleColoredText;
-import com.intellij.ui.SimpleTextAttributes;
+import com.intellij.ui.JBColor;
 
-public class DebugTreeRenderer extends ColoredTreeCellRenderer {
+public class DebugTreeRenderer extends DefaultTreeCellRenderer {
+    private static final String HTML_FORMAT = "<html>%s</html>";
+    private static final String SPAN_FORMAT = "<span style='color:%s;'>%s</span>";
+    private JBColor theColor = null;
+    private String text;
+    private Icon icon;
+
+    @Override
+    public String getText() {
+        return this.text;
+    }
+
+    @Override
+    public Color getBackground() {
+        return theColor;
+    }
+
+    @Override
+    public Color getBackgroundNonSelectionColor() {
+        return theColor;
+    }
+
+    @Override
+    public Icon getIcon() {
+        return this.icon;
+    }
+
+    @Override
+    public Component getTreeCellRendererComponent(JTree tree, Object value, boolean sel, boolean exp, boolean leaf, int row, boolean hasFocus) {
+        super.getTreeCellRendererComponent(tree, value, sel, exp, leaf, row, hasFocus);
+        DebugNode node = (DebugNode) value;
+        StringBuilder sb = new StringBuilder();
+
+        List<String> texts = node.getTexts();
+        List<Integer> colors = node.getColors();
+
+        for (int i = 0; i < texts.size(); i++) {
+            Integer rgb = colors.get(i);
+            Color color = Objects.isNull(rgb) ? Color.lightGray : new Color(rgb);
+            String text = texts.get(i);
+            String hexColor = String.format("#%02X%02X%02X", color.getRed(), color.getGreen(), color.getBlue());
+            sb.append(String.format(SPAN_FORMAT, hexColor, text));
+        }
+        this.text = String.format(HTML_FORMAT, sb);
+
+        if (Objects.nonNull(node.getIconPath())) {
+            this.icon = IconLoader.getIcon(node.getIconPath(), DebugTreeRenderer.class);
+        }
+        return this;
+    }
+}
+
+/*
     @Override
     public void customizeCellRenderer(@NotNull JTree tree, Object value, boolean selected, boolean expanded, boolean leaf, int row, boolean hasFocus) {
         if (!(value instanceof DebugNode)) {
@@ -33,5 +81,4 @@ public class DebugTreeRenderer extends ColoredTreeCellRenderer {
         if (Objects.nonNull(node.getIconPath())) {
             setIcon(IconLoader.getIcon(node.getIconPath(), DebugTreeRenderer.class));
         }
-    }
-}
+    }*/
