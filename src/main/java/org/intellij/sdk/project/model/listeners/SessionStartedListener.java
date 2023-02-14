90b939a788d2ca8b3c9679bc050982ead72804ee
hmaziad <hmaziad@murex.com>
2023-02-14 23:05:58 +0200

    make snap button faster

--- src/main/java/org/intellij/sdk/project/model/listeners/SessionStartedListener.java
+++ src/main/java/org/intellij/sdk/project/model/listeners/SessionStartedListener.java
@@ -3,17 +3,20 @@ package org.intellij.sdk.project.model.listeners;
 import org.intellij.sdk.project.model.components.handlers.ReachServices;
 import com.intellij.debugger.impl.DebuggerManagerListener;
 import com.intellij.debugger.impl.DebuggerSession;
+import com.intellij.ide.ActivityTracker;
 
 public class SessionStartedListener implements DebuggerManagerListener, ReachServices {
 
     @Override
     public void sessionAttached(DebuggerSession session) {
         treeHandler.setSnapEnabled(true, session.getProject());
+        ActivityTracker.getInstance().inc();
     }
 
     @Override
     public void sessionDetached(DebuggerSession session) {
         treeHandler.setSnapEnabled(false, session.getProject());
+        ActivityTracker.getInstance().inc();
     }
 
 }
