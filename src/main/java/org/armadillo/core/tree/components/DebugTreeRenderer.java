5f6fe706f77aab10ce1774e2b93c6bf8409fc69b
hmaziad <hmaziad@murex.com>
2023-03-01 06:52:56 +0200

    add special characters

--- src/main/java/org/armadillo/core/tree/components/DebugTreeRenderer.java
+++ src/main/java/org/armadillo/core/tree/components/DebugTreeRenderer.java
@@ -2,7 +2,9 @@ package org.armadillo.core.tree.components;
 
 
 import java.awt.*;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Objects;
 
 import javax.swing.*;
@@ -11,9 +13,18 @@ import com.intellij.openapi.util.IconLoader;
 
 public class DebugTreeRenderer extends DefaultTreeCellRenderer {
     private static final String HTML_FORMAT = "<html>%s</html>";
-    private static final String SPAN_FORMAT = "<span style='color:%s;'>%s</span>";
+    private static final String SPAN_FORMAT = "<span style=\"color:%s;\">%s</span>";
     private Color theColor = null;
     private final boolean allowColors;
+    private static final Map<Character, String> specialCharacters = new HashMap<>();
+
+    static {
+        specialCharacters.put('&', "&amp;");
+        specialCharacters.put('>', "&gt;");
+        specialCharacters.put('<', "&lt;");
+        specialCharacters.put('"', "&quot;");
+        specialCharacters.put('\'', "&#039;");
+    }
 
     public DebugTreeRenderer(boolean allowColors) {
         this.allowColors = allowColors;
@@ -46,6 +57,7 @@ public class DebugTreeRenderer extends DefaultTreeCellRenderer {
             Color color = Objects.isNull(rgb) ? Color.lightGray : new Color(rgb);
             String text = texts.get(i);
             String hexColor = String.format("#%02X%02X%02X", color.getRed(), color.getGreen(), color.getBlue());
+            text = parseSpecialCharacters(text);
             sb.append(String.format(SPAN_FORMAT, hexColor, text));
         }
         setText(String.format(HTML_FORMAT, sb));
@@ -60,5 +72,13 @@ public class DebugTreeRenderer extends DefaultTreeCellRenderer {
             setIcon(IconLoader.getIcon(node.getIconPath(), DebugTreeRenderer.class));
         }
         return this;
+    }
+
+    private String parseSpecialCharacters(String text) {
+        StringBuilder textBuilder = new StringBuilder();
+        for (char ch : text.toCharArray()) {
+            textBuilder.append(specialCharacters.getOrDefault(ch, String.valueOf(ch)));
+        }
+        return textBuilder.toString();
     }
 }
